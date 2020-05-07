50c9f8cced118dc71240bfb80050596299564cbf
luca-aki <lucax_aks@hotmail.com>
2020-05-08 00:36:03 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name = 'pokedex',
+    version = '0.0.1',
+    packages = ['src'],
+    entry_points = {
+        'console_scripts': [
+            'pokedex = src.__main__:main'
+        ]
+    }
+)
---
+++ src/__main__.py
@@ -0,0 +1,7 @@
+import sys
+
+def main():
+    print("Pokemon:", sys.argv[1])
+
+if __name__ == 'main':
+    main()
